5f6f83b076ff62fb333d8d2665881b0d3f54ec94
paste94 <94paste94@gmail.com>
2019-03-07 14:17:20 +0100

    Versione 0,1. Prima release

--- utilities.js
+++ utilities.js
@@ -1,9 +1,21 @@
+//import 'node_modules/datatables.net-buttons/js/buttons.html5.js';
 const faIcons = require('font-awesome-icons');
 window.$ = window.jQuery = require("jquery")
-var dt = require( 'datatables.net' )();
-var sel = require( 'datatables.net-select' )( );
-var bs4 = require( 'datatables.net-bs4' )( );
-var select = require( 'datatables.net-select-bs4' )( );
+//var dt = require( 'datatables.net' )();
+// https://datatables.net/download/
+var dt = require( 'datatables.net' )( window, $ );
+require( 'jszip' );
+require( 'datatables.net-bs4' )();
+require( 'datatables.net-buttons-bs4' )();
+require( 'datatables.net-buttons/js/buttons.html5.js' )();
+require( 'datatables.net-select-bs4' )();
+
+
+//var sel = require( 'datatables.net-select' )( );
+//var bs4 = require( 'datatables.net-bs4' )( );
+//var select = require( 'datatables.net-select-bs4' )( );
+var popupS = require('popups');
+//require( 'datatables.net-buttons-bs4' )( window, $ );
 var table; // Utilizzata per datatables
 faIcons.getList().then(icons => console.log(icons.length));
 faIcons.getList().then(icons => console.log(icons[0]));
@@ -15,8 +27,8 @@ $(document).ready(function(){
              { data: "Cognome" },
              { data: "Nome" },
              { data: "Data di Nascita" },
-             { data: "Codice Fiscale" }
-
+             { data: "Codice Fiscale" },
+             { data: "Codice Fiscale Società"}
          ]
    }) // Fine DataTable
 }) // Fine function
@@ -31,27 +43,27 @@ $( document ).ready(function() {
 /* Salva il file con i corridori selezionati */
 $(document).ready(function(){
   $('#download-file').click(function(){
-    if(typeof require !== 'undefined') XLSX = require('xlsx');
-    //TODO: Genera il file json dalla tabella
+    var jsonObj = [];
+    table.rows('.selected').every(function(rowIdx, tableLoop, rowLoop){
+      jsonObj.push(this.data()); // Stringa json che rappresenta la riga
+    })
+    if(jsonObj.length > 0){
+      if(typeof require !== 'undefined') XLSX = require('xlsx');
 
-    /*
-    var data = [
-      {"name":"John", "city": "Seattle"},
-      {"name":"Mike", "city": "Los Angeles"},
-      {"name":"Zach", "city": "New York"}
-    ];
-    */
-    /* make the worksheet */
-    var ws = XLSX.utils.json_to_sheet(data);
+      var ws = XLSX.utils.json_to_sheet(jsonObj);
 
-    /* add to workbook */
-    var wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, "People");
-    //TODO: Scegli il path in cui salvare il file
-    XLSX.writeFile(wb, "sheetjs.xlsx");
-    //alert(data);
+      /* add to workbook */
+      var wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, "People");
+      //TODO: Scegli il path in cui salvare il file
+      XLSX.writeFile(wb, "Partecipanti.xlsx");
+      //alert(data);
 
-    //saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), "sheetjs.xlsx");
+      //saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), "sheetjs.xlsx");
+
+    }else {
+      alert('Attenzione, selezionare almeno una riga prima di proseguire!')
+    }
 
   });
 });
@@ -70,31 +82,52 @@ $(document).ready(function () {
 $(document).ready(function(){
     $('input[type="file"]').change(function(e){
         $('#hint').remove();
-        var fileName = e.target.files[0].path;
-        if(typeof require !== 'undefined') XLSX = require('xlsx');
-        var workbook = XLSX.readFile(fileName, {cellDates:true, cellNF:false, cellText:false});
-        var first_sheet_name = workbook.SheetNames[0];
-        var address_of_cell = 'A1';
+        var societyCF;
+        popupS.prompt({
+            content:     'Inserire il codice fiscale della società',
+            onSubmit: function(val) {
+                if(val) {
+                    societyCF = val;
+                    //alert(societyCF)
+                    var fileName = e.target.files[0].path;
+                    if(typeof require !== 'undefined') XLSX = require('xlsx');
+                    var workbook = XLSX.readFile(fileName, {cellDates:true, cellNF:false, cellText:false});
+                    var first_sheet_name = workbook.SheetNames[0];
+                    var address_of_cell = 'A1';
 
-        /* Get worksheet */
-        var worksheet = workbook.Sheets[first_sheet_name];
+                    /* Get worksheet */
+                    var worksheet = workbook.Sheets[first_sheet_name];
 
-        /* Convert all sheet to json object */
-        var jsonString = XLSX.utils.sheet_to_json(worksheet, {dateNF:'YYYY-MM-DD'})
+                    /* Convert all sheet to json object */
+                    var jsonString = XLSX.utils.sheet_to_json(worksheet, {dateNF:'YYYY-MM-DD'})
 
-        /* Riempi tabella */
-        $.each(jsonString, function(i, item){
-          if(item.DataNascita != null){ // Necessario per via di un errore che blocca tutta la funzione
-            table.row.add({
-              'Cognome':          item.Cognome,
-              'Nome':             item.Nome,
-              'Data di Nascita':  item.DataNascita.getDate() + '/' + (item.DataNascita.getMonth() + 1) + '/' + item.DataNascita.getUTCFullYear(),
-              'Codice Fiscale':   item.CodFis
-            }).draw( false );
-          }
-          else{
-            alert('Attenzione: Ad una o più righe manca la data di nascita!')
-          }
+                    /* Riempi tabella */
+                    $.each(jsonString, function(i, item){
+                      if(item.DataNascita != null
+                         && item.Cognome != null
+                         && item.Nome != null
+                         && item.DataNascita != null){ // Necessario per via di un errore che blocca tutta la funzione
+                        table.row.add({
+                          'Cognome':          item.Cognome,
+                          'Nome':             item.Nome,
+                          'Data di Nascita':  item.DataNascita.getDate() + '/' + (item.DataNascita.getMonth() + 1) + '/' + item.DataNascita.getUTCFullYear(),
+                          'Codice Fiscale':   item.CodFis,
+                          'Codice Fiscale Società': societyCF
+                        }).draw( false );
+                      }
+                      /*
+                      else{
+                        alert('Attenzione: Ad una o più righe manca la data di nascita!')
+                      }
+                      */
+                    });
+                } else {
+                    popupS.alert({
+                        content: 'Inserire qualcosa! :('
+                    });
+                }
+                $('#download-file').prop('disabled', false);
+            }
         });
     });
 });
